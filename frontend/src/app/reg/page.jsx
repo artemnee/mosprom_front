e7629d27b51eff0c258bb918cea64c5a50e16d4b
lindeemin <pokerpokers@yandex.ru>
2025-10-18 14:09:18 +0500

    add: registration page

--- frontend/src/app/reg/page.jsx
+++ frontend/src/app/reg/page.jsx
@@ -1,25 +1,25 @@
-import LoginForm from "@/components/LoginForm";
+import RegisterForm from "@/components/RegisterForm";
 
-const index = () => {
+const RegisterPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-slate-50 via-white to-sky-50 px-6">
-      <div className="max-w-5xl w-full grid grid-cols-1  gap-8 items-center">
+      <div className="max-w-5xl w-full grid grid-cols-1 gap-8 items-center">
         <div className="flex items-center justify-center">
           <div className="w-full max-w-md bg-white rounded-2xl shadow-xl p-8">
             <div className="flex flex-col items-center justify-between mb-6">
               <div>
                 <h2 className="text-2xl font-semibold text-slate-800">
-                  Вход в аккаунт
+                  Регистрация
                 </h2>
               </div>
               <div className="text-sm text-slate-400">
-                Нет аккаунта?{" "}
-                <a href="/reg" className="text-sky-600 hover:underline">
-                  Зарегистрироваться
+                Уже есть аккаунт?{" "}
+                <a href="/login" className="text-sky-600 hover:underline">
+                  Войти
                 </a>
               </div>
             </div>
-            <LoginForm />
+            <RegisterForm />
           </div>
         </div>
       </div>
@@ -27,4 +27,4 @@ const index = () => {
   );
 };
 
-export default index;
+export default RegisterPage;
